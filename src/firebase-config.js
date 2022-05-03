285d4aad12f4d5b68fd351b317f6f1e73b91279b
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-03 16:06:32 -0400

    env keys and export db

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 require("dotenv").config();
 const {
   API_KEY,
@@ -8,14 +14,6 @@ const {
   APP_ID,
   MEASUREMENT_ID,
 } = process.env;
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
   apiKey: API_KEY,
@@ -29,3 +27,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
