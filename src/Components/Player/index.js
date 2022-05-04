02628427ef134f2f1b9337da172602927d7ddf73
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-04 16:29:58 -0400

    Player component setup

--- src/Components/Player/index.js
+++ src/Components/Player/index.js
@@ -1,7 +1,21 @@
-import React from "react";
+import { useState } from "react";
 
-function Player() {
-  return <div>Player</div>;
+function Player({ _url, title }) {
+  const url =
+    "https://firebasestorage.googleapis.com/v0/b/podfast-432ab.appspot.com/o/recordings%2Fdc1.mp3?alt=media&token=1ab2f7d5-ab62-4ea6-9d82-e9b12917fd74";
+  const [play, setPlay] = useState(url);
+
+  return (
+    <>
+      <h2>title: {title}</h2>
+      <audio
+        controls
+        src={play}
+        autoPlay
+        onEnded={() => console.log("the song has ended")}
+      ></audio>
+    </>
+  );
 }
 
 export default Player;
