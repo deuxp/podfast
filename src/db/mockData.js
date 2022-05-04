7010045618843170b08f44ee17e8c00c5afc5078
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-04 16:29:11 -0400

    mock data

---
+++ src/db/mockData.js
@@ -0,0 +1,28 @@
+const minicasts = [
+  {
+    active: true,
+    audio_link:
+      "https://firebasestorage.googleapis.com/v0/b/podfast-432ab.appspot.com/o/recordings%2Fdc1.mp3?alt=media&token=1ab2f7d5-ab62-4ea6-9d82-e9b12917fd74",
+    banner_link: "",
+    category_id: "mystery",
+    description: "test song slower",
+    title: "df1",
+    user_id: "lmKhbyqPDL4ttyTNxsM6",
+    views: 0,
+    created_at: "May 3, 2022 at 12:00:00 AM UTC-4",
+  },
+  {
+    active: true,
+    audio_link:
+      "https://firebasestorage.googleapis.com/v0/b/podfast-432ab.appspot.com/o/ackeeTree.mp3?alt=media&amp;token=9efd3b6b-86d8-4474-b6be-ef6a6618e960",
+    banner_link: "",
+    category_id: "comedy",
+    description: "beats",
+    title: "tree_shaking",
+    user_id: "lmKhbyqPDL4ttyTNxsM6",
+    views: 0,
+    created_at: "May 4, 2022 at 4:10:00 AM UTC-4",
+  },
+];
+
+export default minicasts;
