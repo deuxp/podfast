f5f7025930588c325bc9ac83dd8a138822d698f4
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-05 10:34:23 -0400

    redundant import

--- src/Components/Player/index.js
+++ src/Components/Player/index.js
@@ -1,10 +1,4 @@
-import { useState } from "react";
-
 function Player({ play, playNextSong, helper }) {
-  // const url =
-  //   "https://firebasestorage.googleapis.com/v0/b/podfast-432ab.appspot.com/o/recordings%2Fdc1.mp3?alt=media&token=1ab2f7d5-ab62-4ea6-9d82-e9b12917fd74";
-  // const [play, setPlay] = useState(url);
-
   const playsTheFirstOneOnly = (list) => {
     if (list[0]) {
       return list[0].audio_link;
