c463e4aedad03a220baf9597ab486c5b47196b43
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-04 21:48:08 -0400

    buildlist functions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import React from "react";
 
 function App() {
   const [state, setState] = useState({});
+  // plays the next song on state change, feed it the next song
+  const [song, setSong] = useState();
 
   // initial get from the server (mocked for now from import "./db/mockData")
   useEffect(() => {
@@ -18,10 +20,49 @@ function App() {
   }, []);
 
   /* ----------------------------- helper function ---------------------------- */
+  console.log(state);
+  // function to build a short list of casts to listen to on the front page
+  // as of now just makes a list, no filter, impose a limit 6
+  const buildList = (state, filter) => {
+    //implement lazy loading
+    return new Promise((resolve, reject) => {
+      const shortList = state.minicasts; // used to build the articles
+      resolve(shortList);
+    }).catch((e) => console.log(e));
+  };
+
+  // list of urls for the player to consume
+  const ulrList = (shortList) => {
+    if (!shortList) return [];
+    return shortList.map((cast) => cast.audio_link); // audio_link refering to the url of song
+  };
+
+  // [].length = 8
+  // console;
+
+  const playNextSong = (next) => {
+    // destroy list entry of played
+
+    // set next play
+    setSong(next);
+  };
+
+  buildList(state)
+    .then((shortlist) => {
+      return shortlist;
+    })
+    .then((shortList) => {
+      return ulrList(shortList);
+    })
+    .then((urls) => {
+      // do something with the urls
+      playNextSong(urls[0]);
+    });
+  // expect array of two urls
 
   return (
     <>
-      <Player />
+      <Player play={song} playNextSong={() => playNextSong()} />
       <Minicast />
     </>
   );
--- src/Components/Player/index.js
+++ src/Components/Player/index.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-function Player({ _url, title }) {
-  const url =
-    "https://firebasestorage.googleapis.com/v0/b/podfast-432ab.appspot.com/o/recordings%2Fdc1.mp3?alt=media&token=1ab2f7d5-ab62-4ea6-9d82-e9b12917fd74";
-  const [play, setPlay] = useState(url);
+function Player({ play, title }) {
+  // const url =
+  //   "https://firebasestorage.googleapis.com/v0/b/podfast-432ab.appspot.com/o/recordings%2Fdc1.mp3?alt=media&token=1ab2f7d5-ab62-4ea6-9d82-e9b12917fd74";
+  // const [play, setPlay] = useState(url);
 
   return (
     <>
