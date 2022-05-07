3dc7224578b90287422d6f760f507a30756563c7
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-07 00:22:56 -0400

    structure the dash

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Container } from "@mui/material";
+import Recorder from "../Recorder";
+import DashCastList from "../DashCastList";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Container maxWidth="sm">
+      <Recorder />
+      <DashCastList />
+    </Container>
+  );
 }
 
 export default Dashboard;
