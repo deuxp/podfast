3e1f1b5f0828db1733d0357c10bccd75565b5ea3
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-07 00:24:07 -0400

    building recorder

---
+++ src/Components/Recorder/index.js
@@ -0,0 +1,49 @@
+import Box from "@mui/material/Box";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import Fab from "@mui/material/Fab";
+import SettingsVoiceIcon from "@mui/icons-material/SettingsVoice";
+import StopCircleIcon from "@mui/icons-material/StopCircle";
+import PausePresentationIcon from "@mui/icons-material/PausePresentation";
+
+// font-family: 'Goldman', cursive; <-- the typography css rule
+
+function Recorder() {
+  return (
+    <>
+      <h2>Record a mini-cast</h2>
+      <Box
+        sx={{
+          borderRadius: "15px",
+          width: "40vw",
+          height: 300,
+          backgroundColor: "rgba(104, 17, 216, 1)",
+          "&:hover": {
+            backgroundColor: "rgba(104, 17, 216, 1)",
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      >
+        <Fab
+          aria-label="add"
+          sx={{
+            backgroundColor: "rgba(0, 255, 240, 1)",
+            border: "solid rgba(147, 246, 223, 1)",
+          }}
+        >
+          <SettingsVoiceIcon />
+        </Fab>
+        <Fab
+          aria-label="add"
+          sx={{
+            backgroundColor: "rgba(0, 255, 240, 1)",
+            border: "solid rgba(147, 246, 223, 1)",
+          }}
+        >
+          <StopCircleIcon />
+        </Fab>
+      </Box>
+    </>
+  );
+}
+
+export default Recorder;
