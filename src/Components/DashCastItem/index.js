f48e7e42dbe994447225464dc89460f95f5a6c3c
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-09 13:19:21 -0400

    layout structure

--- src/Components/DashCastItem/index.js
+++ src/Components/DashCastItem/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Box from "@mui/material/Box";
 
-function DashCastItem({ casts }) {
-  return <div>{casts}</div>;
+function DashCastItem({ cast }) {
+  // bring in the data
+
+  const renderedArticle = (
+    <Box
+      component="span"
+      sx={{
+        width: "100%",
+        height: 300,
+        backgroundColor: "primary.dark",
+        "&:hover": {
+          backgroundColor: "primary.main",
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    >
+      <p>Save</p>
+    </Box>
+  );
+
+  return <>{renderedArticle}</>;
 }
 
 export default DashCastItem;
--- src/Components/DashCastList/index.js
+++ src/Components/DashCastList/index.js
@@ -1,11 +1,29 @@
 import DashCastItem from "../DashCastItem";
-const dashList = [1, 2, 3, 4, 5];
-const renderItem = dashList.map((cast, index) => (
-  <DashCastItem key={index} cast={cast} />
-));
+import Stack from "@mui/material/Stack";
+import ListItem from "@mui/material/ListItem";
+import Paper from "@mui/material/Paper";
+
+import Container from "@mui/material/Container";
+import { useState } from "react";
+
+const dashList = ["1", "2", "3", "4", "5"];
+
+//TODO Bring in the data:
 
 function DashCastList() {
-  return <>{renderItem}</>;
+  const [userMiniCasts, setUserMiniCasts] = useState([]);
+
+  const renderItem = dashList.map((cast, index) => (
+    <ListItem key={index}>
+      <DashCastItem cast={cast} />
+    </ListItem>
+  ));
+  return (
+    <Container maxWidth="md">
+      <h3> Your Minicasts:</h3>
+      <Stack>{renderItem}</Stack>
+    </Container>
+  );
 }
 
 export default DashCastList;
