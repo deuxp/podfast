59a4f68af523ef92b010d4dca683ac3bb16813d4
Alison Harman <alison_harman@sympatico.ca>
2022-05-10 18:47:21 -0400

    set onClick to setCurrentCast

--- src/Components/Minicast/index.js
+++ src/Components/Minicast/index.js
@@ -5,19 +5,23 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
-import { Avatar, CardHeader } from "@mui/material";
+import { Avatar, CardActionArea, CardHeader } from "@mui/material";
 
 
 function Minicast(props) {
 
   return (
-    <Card 
+    <Card
       sx={{ maxWidth: 700, m: "1rem" }}
       variant="outlined"
     >
+      <CardActionArea
+      onClick={props.setCurrentCast}
+      >
       <CardHeader
         avatar={<Avatar src={props.avatar_link}></Avatar>}
         title={<Typography variant="h5">{props.title}</Typography>}
+        subheader={props.handle}
       >
       </CardHeader>
       <CardMedia
@@ -26,13 +30,14 @@ function Minicast(props) {
         height="140"
         image={props.banner_link}
       />
+      </CardActionArea>
       <CardContent>
         <Typography variant="body2" color="text.secondary">
           {props.description}
         </Typography>
       </CardContent>
       <CardActions>
-  
+
         <Button size="small">Like</Button>
         <Button size="small">Share</Button>
         <Button size="small">Follow</Button>
