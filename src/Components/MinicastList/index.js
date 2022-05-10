cfcd69a526c8e12d352ab3bfe914de879a216117
Alison Harman <alison_harman@sympatico.ca>
2022-05-10 16:59:18 -0400

    add unique to each Minicast component and added avatar_link as props

--- src/Components/MinicastList/index.js
+++ src/Components/MinicastList/index.js
@@ -2,9 +2,9 @@ import Minicast from "../Minicast";
 
 function MinicastList(props) {
 
-  const MinicastArray = props.minicasts.map(minicast => {
+  const MinicastArray = props.minicasts.map( (minicast, index) => {
     return (
-      <Minicast key={minicast.user_id} title={minicast.title} description={minicast.description} audio_link={minicast.audio_link} banner_link={minicast.banner_link} />
+      <Minicast key={index} title={minicast.title} description={minicast.description} audio_link={minicast.audio_link} banner_link={minicast.banner_link} avatar_link={minicast.avatar_link}/>
     )
   })
   
