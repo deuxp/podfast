8dababfe17259af24a5bb41b4c6be4ce241b2d3f
Alison Harman <alison_harman@sympatico.ca>
2022-05-11 13:22:18 -0400

    put everything but Nav into MUI Container component and start creating the playlist view on side

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "./App.scss";
 import Player from "./Components/Player";
-import minicasts from "./db/mockData";
 import MinicastList from "./Components/MinicastList";
 import Nav from "./Components/Nav";
 import Dashboard from "./Components/Dashboard";
 
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-//import Typography from "@mui/material/Typography";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-//import Button from '@mui/material/Button';
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 
 import axios from "axios";
+import { Container } from "@mui/material";
+import { InsertEmoticonRounded, InsertEmoticonTwoTone } from "@mui/icons-material";
 
 const theme = createTheme({
   palette: {
@@ -46,27 +45,25 @@ function App() {
 
   const GET_URL = "http://localhost:8080/minicasts";
 
-  // initial get from the server (mocked for now from import "./db/mockData")
+  // initial get from the server 
   useEffect(() => {
     axios
-    .get(GET_URL)
-    .then((res) => {
-      return res.data
-    })
-    .then((res) => {
-      setState({
-        ...state,
-        minicasts: res, // [{}] array of objects
+      .get(GET_URL)
+      .then((res) => {
+        return res.data
+      })
+      .then((res) => {
+        setState({
+          ...state,
+          minicasts: res, // [{}] array of objects
+        });
+      })
+      .catch((e) => {
+        console.log(e.message);
       });
-    })
-    .catch((e) => {
-      console.log(e.message);
-    });
   }, []);
 
-
  
-
   /* ----------------------------- helper function ---------------------------- */
 
   // //function to build a short list of casts to listen to on the front page
@@ -109,61 +106,72 @@ function App() {
   };
 
   return (
+
     <div className="App">
+
       <ThemeProvider theme={theme}>
         <Nav />
       </ThemeProvider>
+      <Container>
+        <div className="main-grid">
+          <div className="player-box">
+            <section className="console">
+              <Player
+                play={playlist}
+                currentCast={currentCast}
+                playNextSong={() => playNextSong(playlist)}
+              />
+            </section>
+          </div>
 
-      <div className="main-grid">
-        <div className="player-box">
-          <section className="console">
-            <Player
-              play={playlist}
-              currentCast={currentCast}
-              playNextSong={() => playNextSong(playlist)}
-            />
-          </section>
-        </div>
+          <div className="main-box">
+            <section className="minicasts-dashboard">
+              {!dashboard && <MinicastList minicasts={playlist} onChange={setCurrentCast} />}
+              {dashboard && <Dashboard />}
+            </section>
+          </div>
 
-        <div className="menu-box">
-          <section className="minicasts-dashboard">
-            {!dashboard && <MinicastList minicasts={playlist} onChange={setCurrentCast} />}
-            {dashboard && <Dashboard />}
-          </section>
+          <div className="menu-box">
+            <Box
+              sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+            >
+              <Toolbar />
+              <List>
+                <ListItemButton
+                  key="Profile"
+                  selected={selectedIndex === 0}
+                  onClick={(event) => handleListItemClick(event, 0)}
+                >
+                  <ListItemText primary="Profile" />
+                </ListItemButton>
+                <ListItemButton
+                  key="Dashboard"
+                  selected={selectedIndex === 1}
+                  onClick={(event) => handleListItemClick(event, 1)}
+                >
+                  <ListItemText primary="Dashboard" />
+                </ListItemButton>
+                <ListItemButton
+                  key="Following"
+                  selected={selectedIndex === 2}
+                  onClick={(event) => handleListItemClick(event, 2)}
+                >
+                  <ListItemText primary="Following" />
+                </ListItemButton>
+              </List>
+              <Divider />
+            </Box>
+            <Box>
+              <strong>Playlist</strong>
+              <ul>
+                {state.minicasts && !dashboard ? (state.minicasts.map((item, index) => {
+                  return (<li key={index}>{item.title}</li>)
+                })) : "" }
+              </ul>
+            </Box>
+          </div>
         </div>
-
-        <div className="main-box">
-          <Box
-            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-          >
-            <Toolbar />
-            <List>
-              <ListItemButton
-                key="Profile"
-                selected={selectedIndex === 0}
-                onClick={(event) => handleListItemClick(event, 0)}
-              >
-                <ListItemText primary="Profile" />
-              </ListItemButton>
-              <ListItemButton
-                key="Dashboard"
-                selected={selectedIndex === 1}
-                onClick={(event) => handleListItemClick(event, 1)}
-              >
-                <ListItemText primary="Dashboard" />
-              </ListItemButton>
-              <ListItemButton
-                key="Following"
-                selected={selectedIndex === 3}
-                onClick={(event) => handleListItemClick(event, 3)}
-              >
-                <ListItemText primary="Following" />
-              </ListItemButton>
-            </List>
-            <Divider />
-          </Box>
-        </div>
-      </div>
+      </Container>
     </div>
   );
 }
