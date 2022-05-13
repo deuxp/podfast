6a7eb0b06f6cbbfe5e0330deacc7b338c3a83704
Alison Harman <alison_harman@sympatico.ca>
2022-05-13 14:37:47 -0400

    experiment with React Routing on Menu List items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import * as React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 /*************************Poodle Logo Pic*************************** */
 import Poodle from "../src/assets/PoodleGraphic.png"
 /****************************** CSS *********************************** */
 import "./App.scss";
+/****************************** React Router *********************************** */
+import { BrowserRouter, Routes, Route, NavLink, MemoryRouter, Link as RouterLink } from "react-router-dom";
+import { StaticRouter } from 'react-router-dom/server';
 /*************************Custom Components*************************** */
 import Player from "./Components/Player";
 import MinicastList from "./Components/MinicastList";
 import Nav from "./Components/Nav";
 import Dashboard from "./Components/Dashboard";
 /************************** MUI Components***************************************** */
+import ListItemIcon from '@mui/material/ListItemIcon';
 import {
   Box,
   Toolbar,
@@ -28,6 +32,8 @@ import {
   Container
 } from './mui';
 
+
+
 const theme = createTheme({
   palette: {
     primary: {
@@ -44,6 +50,40 @@ const theme = createTheme({
     },
   },
 });
+
+const LinkBehavior = React.forwardRef((props, ref) => (
+  <RouterLink ref={ref} to="/" {...props} role={undefined} />
+));
+
+function Router(props) {
+  const { children } = props;
+  if (typeof window === 'undefined') {
+    return <StaticRouter location="/">{children}</StaticRouter>;
+  }
+
+  return <MemoryRouter>{children}</MemoryRouter>;
+}
+
+function ListItemLink(props) {
+  const { icon, primary, to, button } = props;
+
+  const renderLink = React.useMemo(
+    () =>
+      React.forwardRef(function Link(itemProps, ref) {
+        return <RouterLink to={to} ref={ref} {...itemProps} role={undefined} />;
+      }),
+    [to],
+  );
+
+  return (
+    <li>
+      <ListItem button={button} component={renderLink}>
+        {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+        <ListItemText primary={primary} />
+      </ListItem>
+    </li>
+  );
+}
 
 function App() {
   const [playlist, setPlaylist] = useState([]); // gonna be a static list moving forward
@@ -70,6 +110,14 @@ function App() {
   }, []);
 
 
+  const renderLink = React.useMemo(
+    () =>
+      React.forwardRef(function Link(itemProps, ref) {
+        return <RouterLink to="/dashboard" ref={ref} {...itemProps} role={undefined} />;
+      }),
+    ["/dashboard"],
+  );
+
   const handleListItemClick = (_event, index) => {
     setSelectedIndex(index);
     if (index === 1) setDashboard(true);
@@ -82,84 +130,103 @@ function App() {
 
   return (
 
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
 
-      <ThemeProvider theme={theme}>
-        <Nav />
+        <ThemeProvider theme={theme}>
+          <Nav />
 
-        <Container>
-          <div className="main-grid">
-            <div className="player-box">
-              <Player
-                playlist={playlist}
-                autoplay={autoplay}
-                currentCast={currentCast}
-                onEnded={() => setCurrentCast(currentCast + 1)}
-              />
-            </div>
+          <Container>
+            <div className="main-grid">
+              <div className="player-box">
+                <Player
+                  playlist={playlist}
+                  autoplay={autoplay}
+                  currentCast={currentCast}
+                  onEnded={() => setCurrentCast(currentCast + 1)}
+                />
+              </div>
+              <div className="main-box">
+                <Routes>
+                  <Route path="*" element={<MinicastList minicasts={playlist} onChange={setCurrentCast} />} />
+                  <Route exact path="/dashboard" element={<Dashboard />} />
+                </Routes>
+              </div>
 
-            <div className="main-box">
-              {!dashboard && <MinicastList minicasts={playlist} onChange={setCurrentCast} />}
-              {dashboard && <Dashboard />}
-            </div>
+              {/* <div className="main-box">
+                {!dashboard && <MinicastList minicasts={playlist} onChange={setCurrentCast} />}
+                {dashboard && <Dashboard />}
+  </div> */}
 
-            <div className="side-bar">
-              <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}    >
-                <Toolbar />
-                <List>
 
-                  <ListItem>
-                    <ListItemText id="switch-list-label-autoplay" primary="Autoplay" />
-                    <Switch
-                      checked={autoplay}
-                      onChange={handleAutoPlaySwitch}
-                      inputProps={{ 'aria-label': 'controlled' }}
+              <div className="side-bar">
+                <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}    >
+                  <Toolbar />
+                  <List>
+
+                    <ListItem>
+                      <ListItemText id="switch-list-label-autoplay" primary="Autoplay" />
+                      <Switch
+                        checked={autoplay}
+                        onChange={handleAutoPlaySwitch}
+                        inputProps={{ 'aria-label': 'controlled' }}
+                      />
+                    </ListItem>
+                    <Divider />
+
+                    <ListItemButton
+                      key="Home"
+                      selected={selectedIndex === 0}
+                      onClick={(event) => handleListItemClick(event, 0)}
+                    >
+                      <ListItemText primary="Home" />
+                    </ListItemButton>
+
+                    <ListItemLink
+                      to="/dashboard"
+                      primary="Dashboard2"
+                      icon={null}
+                      button={true}
+                      key="Dashboard2"
+                      selected={selectedIndex === 1}
+                      onClick={(event) => handleListItemClick(event, 1)}
                     />
-                  </ListItem>
-                  <Divider />
 
-                  <ListItemButton
-                    key="Home"
-                    selected={selectedIndex === 0}
-                    onClick={(event) => handleListItemClick(event, 0)}
-                  >
-                    <ListItemText primary="Home" />
-                  </ListItemButton>
-
-                  <ListItemButton
-                    key="Dashboard"
-                    selected={selectedIndex === 1}
-                    onClick={(event) => handleListItemClick(event, 1)}
-                  >
-                    <ListItemText primary="Dashboard" />
-                  </ListItemButton>
-                  <Divider />
-
-                  {!dashboard ? (<><ListItemButton onClick={() => setOpen(!open)}>
-                    <ListItemText primary="Playlist" />
-                    {open ? <ExpandLess /> : <ExpandMore />}
-                  </ListItemButton>
-                    <Collapse in={open} unmountOnExit>
-                      <ul>
-                        {playlist.map((item, index) => {
-                          return (<li key={index}>{item.title}</li>)
-                        })}
-                      </ul>
-                    </Collapse>
-                    <ListItemButton>
-                      <ListItemText primary="Favorites" />
-                    </ListItemButton> </>) : ""}
-                  <Divider />
-                </List>
-              </Box>
-              <div>
-                <img src={Poodle} />
+                    <ListItemButton
+                      key="Dashboard"
+                      selected={selectedIndex === 1}
+                      onClick={(event) => handleListItemClick(event, 1)}
+                      component={renderLink}
+                    >
+                      <ListItemText primary="Dashboard" />
+                    </ListItemButton>
+                    <Divider />
+                    {!dashboard ? (<><ListItemButton onClick={() => setOpen(!open)}>
+                      <ListItemText primary="Playlist" />
+                      {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemButton>
+                      <Collapse in={open} unmountOnExit>
+                        <ul>
+                          {playlist.map((item, index) => {
+                            return (<li key={index}>{item.title}</li>)
+                          })}
+                        </ul>
+                      </Collapse>
+                      <ListItemButton>
+                        <ListItemText primary="Favorites" />
+                      </ListItemButton> </>) : ""}
+                    <Divider />
+                  </List>
+                </Box>
+                <div>
+                  <img src={Poodle} />
+                </div>
               </div>
             </div>
-          </div>
-        </Container>
-      </ThemeProvider>
-    </div>
+          </Container>
+        </ThemeProvider>
+      </div>
+    </BrowserRouter>
   );
 }
 
