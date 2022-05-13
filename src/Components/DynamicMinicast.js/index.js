a4d9d6e3da449b1b071804cf0eadb1f9a40ac869
Alison Harman <alison_harman@sympatico.ca>
2022-05-13 17:12:38 -0400

    create new Component to display individual linked minicasts in case we want to add further data, like creator profile

---
+++ src/Components/DynamicMinicast.js/index.js
@@ -0,0 +1,17 @@
+import Minicast from "../Minicast";
+import { useParams } from 'react-router-dom'
+
+
+function DynamicMinicast(props) {
+
+  const { id } = useParams();
+  // I believe I should GET the one minicast of interest from the data base rather than choosing index based on route parameter and passing all the playlists
+  let minicast = props.minicasts[id];
+
+  return (
+    <Minicast key={id} title={minicast.title} description={minicast.description} audio_link={minicast.audio_link} banner_link={minicast.banner_link} avatar_link={minicast.avatar_link} setCurrentCast={props.onChange(id) } handle={minicast.handle} /> 
+    )
+  
+};
+
+export default DynamicMinicast;
