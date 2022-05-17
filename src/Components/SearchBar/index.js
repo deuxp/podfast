31378e01efdcf687fa20204eedfc6905c94ba758
Alison Harman <alison_harman@sympatico.ca>
2022-05-17 14:42:20 -0400

    add Tooltip feature that when user hovers over searchbar it displays possible categories to search

--- src/Components/SearchBar/index.js
+++ src/Components/SearchBar/index.js
@@ -2,25 +2,61 @@ import { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
+import { Tooltip } from "@mui/material";
 
 
-export default function SearchBar({ setSearchQuery }) {
+export default function SearchBar(props) {
+
+  // hardcoded category data but need to swap this for a get request of the categories
+  const data = [
+    "comedy",
+    "education",
+    "history",
+    "lifestyle",
+    "music",
+    "news",
+    "science",
+    "trailers",
+  ];
+
+  const filterData = (query, data) => {
+    if (!query) {
+      return data;
+    } else {
+      return data.filter((d) => d.toLowerCase().includes(query));
+    }
+  };
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const dataFiltered = filterData(searchQuery, data);
+
   return (
+
     <form  >
-      <TextField
-        id="search-bar"
-        className="text"
-        onInput={(e) => {
-          setSearchQuery(e.target.value);
-        }}
-        variant="outlined"
-        placeholder="#trending"
-        size="small"
-        style={{ background: "white", borderRadius: "20px", minWidth: "300px" }}
-      />
+      <Tooltip
+        disableFocusListener
+        disableTouchListener
+        arrow
+        title={dataFiltered.map(d => (
+          <p>{d}</p>
+        ))}
+      >
+        <TextField
+          id="search-bar"
+          className="text"
+          onInput={(e) => {
+            setSearchQuery(e.target.value);
+          }}
+          variant="outlined"
+          placeholder="#trending"
+          size="small"
+          style={{ background: "white", borderRadius: "20px", minWidth: "300px" }}
+        />
+      </Tooltip>
       <IconButton type="submit" aria-label="search">
         <SearchIcon style={{ fill: "blue" }} />
       </IconButton>
     </form>
+
   )
 };
