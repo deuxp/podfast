c0f08784f83cb9da5330522b30a1b73e9c59805b
Alison Harman <alison_harman@sympatico.ca>
2022-05-17 00:04:58 -0400

    add GET to component so that it is possible to copy link and display in other window

--- src/Components/DynamicMinicast.js/index.js
+++ src/Components/DynamicMinicast.js/index.js
@@ -1,15 +1,35 @@
 import Minicast from "../Minicast";
 import { useParams } from 'react-router-dom'
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 
 function DynamicMinicast(props) {
 
-  const { id } = useParams();
+  let { id } = useParams();
   // I believe I should GET the one minicast of interest from the data base rather than choosing index based on route parameter and passing all the playlists
-  let minicast = props.minicasts[id];
+
+  const [minicast, setMinicast] = useState([])
+
+  const GET_URL = `http://localhost:8080/minicasts/`;
+
+  // get from the server 
+  // unfortunately have to get all minicasts as the id refers to the order in which they come from the server 
+  // will rewrite this hook to be reusable among different components
+  useEffect(() => {
+    axios
+      .get(GET_URL)
+      .then((res) => {
+        setMinicast(res.data[id])
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, [id]);
+  
 
   return (
-    <Minicast key={id} id={id} title={minicast.title} description={minicast.description} audio_link={minicast.audio_link} banner_link={minicast.banner_link} avatar_link={minicast.avatar_link} setCurrentCast={props.onChange(id) } handle={minicast.handle} /> 
+    <Minicast key={minicast.id} id={id} title={minicast.title} description={minicast.description} audio_link={minicast.audio_link} banner_link={minicast.banner_link} avatar_link={minicast.avatar_link} setCurrentCast={props.onChange(id) } handle={minicast.handle} /> 
     )
   
 };
