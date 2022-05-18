fa0c53db63305e02a3d24e16412db49574d70670
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-18 15:59:40 -0400

    border for the avatar

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -52,7 +52,11 @@ export default function Nav({ setUserID }) {
           </Typography>
 
           <Button variant="outlined" onClick={handleClickOpen}>
-            <Avatar src={user?.avatar_link} onClick={handleClickOpen} />
+            <Avatar
+              src={user?.avatar_link}
+              onClick={handleClickOpen}
+              sx={{ border: "solid rgba(0, 255, 240, 1)" }}
+            />
           </Button>
 
           {loggedIn && (
