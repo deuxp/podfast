7974a89a60712c678bb8d3b8a1c994d419b85977
Alison Harman <alison_harman@sympatico.ca>
2022-05-17 23:04:28 -0400

    onEnded event will now calculate nextCast with player based on playlist and currentCast

--- src/Components/Player/index.js
+++ src/Components/Player/index.js
@@ -1,4 +1,4 @@
-function Player({ playlist, currentCast, autoplay, onEnded }) {
+function Player({ playlist, currentCast, autoplay,  setCurrentCast }) {
 
   const playsSelectedCast = (list, index) => {
     if (list[index]) {
@@ -7,10 +7,33 @@ function Player({ playlist, currentCast, autoplay, onEnded }) {
     return "";
   };
 
+  const nextCast = (currentCast, playlist) => {
+    // if I don't have this logic, the app crashes
+    if (!currentCast) {
+      return "";
+    }
+    // find index of currentCast given current playlist so I can choose next playlist object for next cast
+    let index = 0;
+    for (let i= 0; i < playlist.length; i++) {
+      if (playlist[i].id === currentCast.id) {
+        index = i;
+      }
+    }
+
+    if (playlist[index+1]) {
+      return playlist[index+1]
+    }
+    // if no further casts, then can repeat the last one
+    else
+     {return currentCast;}
+  }
+  
+  let next = nextCast(currentCast, playlist);
+
   return (
     <>
-      <h2>{playlist[currentCast] ? playlist[currentCast].title : ""}</h2>
-      <audio controls autoPlay={autoplay} src={playsSelectedCast(playlist, currentCast)} onEnded={onEnded} ></audio>
+      <h2>{currentCast ? currentCast.title : ""}</h2>
+      <audio controls autoPlay={autoplay} src={currentCast?.audio_link} onEnded={() => setCurrentCast(next)} ></audio>
     </>
   );
 }
