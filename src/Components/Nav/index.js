153f133dee97a1ea1f822e5ee976c12a5ba2f4a7
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-18 10:56:09 -0400

    implement user logout

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,4 +1,5 @@
 import "./Nav.scss";
+import { useState } from "react";
 import Login from "../Login";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import Dialog from "@mui/material/Dialog";
@@ -8,6 +9,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { Avatar } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export default function Nav({ setUserID }) {
   const {
@@ -23,7 +25,14 @@ export default function Nav({ setUserID }) {
     handleLogin,
     handleRegister,
     errorMessage,
+    handleLogout,
+    loggedIn,
   } = useUserAuth();
+
+  const logoutUser = () => {
+    handleLogout();
+    setUserID("");
+  };
 
   return (
     <Box sx={{ width: 1 }}>
@@ -37,25 +46,46 @@ export default function Nav({ setUserID }) {
           >
             PodFast
           </Typography>
+
           <Button variant="outlined" onClick={handleClickOpen}>
             <Avatar src="" onClick={handleClickOpen} />
           </Button>
 
-          <Dialog open={open} onClose={handleClickClose}>
-            <Login
-              handleClickClose={handleClickClose}
-              password={password}
-              setPassword={setPassword}
-              email={email}
-              setEmail={setEmail}
-              handleLogin={handleLogin}
-              handleRegister={handleRegister}
-              setRegister={setRegister}
-              register={register}
-              setUserSession={setUserID}
-              errorMessage={errorMessage}
-            />
-          </Dialog>
+          {loggedIn && (
+            <Box
+              onClick={logoutUser}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                opacity: "0.5",
+                "&:hover": {
+                  cursor: "pointer",
+                  color: "rgba(0, 255, 240, 1)",
+                },
+              }}
+            >
+              <LogoutIcon />
+              <Typography variant="caption">logout</Typography>
+            </Box>
+          )}
+
+          {!loggedIn && (
+            <Dialog open={open} onClose={handleClickClose}>
+              <Login
+                handleClickClose={handleClickClose}
+                password={password}
+                setPassword={setPassword}
+                email={email}
+                setEmail={setEmail}
+                handleLogin={handleLogin}
+                handleRegister={handleRegister}
+                setRegister={setRegister}
+                register={register}
+                setUserSession={setUserID}
+                errorMessage={errorMessage}
+              />
+            </Dialog>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
