c7ee97fbccf9e51fdca9baa1f1dba7303aea44a0
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-22 15:24:01 -0400

    avatar size restrictions

--- src/Components/CreatorShow/index.js
+++ src/Components/CreatorShow/index.js
@@ -36,10 +36,10 @@ function CreatorShow({ creator }) {
                 alt="Avatar"
                 src={creator.avatar_link}
                 sx={{
-                  transform: "scale(4)",
+                  transform: "scale(3)",
                   borderRadius: "30px",
-                  maxHeight: "30px",
-                  maxWidth: "30px",
+                  maxHeight: "40px",
+                  maxWidth: "40px",
                   border: "solid #6811d8",
                 }}
               />
